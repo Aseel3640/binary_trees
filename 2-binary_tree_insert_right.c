146eca57f6fa6e3a3286cb3d8b45cb674f382ea0
Aseel3640 <aseelsaifaldin@yahoo.com>
2023-03-29 02:05:32 -0700

    -

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,29 +8,25 @@
  *
  * Return: If parent is NULL or an error occurs - NULL.
  *         Otherwise - a pointer to the new node.
- *
- * Description: If parent already has a right-child, the new node
- *              takes its place and the old right-child is set as
- *              the right-child of the new node.
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *new_node;
 
-	if (parent == NULL)
+	if (!parent)
 		return (NULL);
 
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
 		return (NULL);
 
-	if (parent->right != NULL)
+	if (parent->right == NULL)
+		parent->right = new_node;
+	else
 	{
-		new->right = parent->right;
-		parent->right->parent = new;
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+		parent->right = new_node;
 	}
-	parent->right = new;
-
-	return (new);
+	return (new_node);
 }
-
